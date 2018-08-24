5389b21110ff59f863186684c010252f0c67fc18
marunagahideo <hidoooo@gmail.com>
2018-08-24 19:36:47 +0900

    feat(packages): add task that optimize image for gulp

---
+++ packages/gulp-task-optimize-image/src/index.js
@@ -0,0 +1,85 @@
+import {src, dest} from 'gulp';
+import plumber from 'gulp-plumber';
+import cond from 'gulp-if';
+import filter from 'gulp-filter';
+import imagemin from 'gulp-imagemin';
+import gzip from 'gulp-gzip';
+import evenizer from '@hidoo/gulp-plugin-image-evenizer';
+import errorHandler from '@hidoo/gulp-util-error-handler';
+
+/**
+ * task default options.
+ * @type {Object}
+ */
+const DEFAULT_OPTIONS = {
+
+  // source path (required)
+  src: null,
+
+  // destination path (required)
+  dest: null,
+
+  // apply evenize or not
+  evenize: false,
+
+  // evenize options
+  evenizeOptions: {
+    imageMagick: true
+  },
+
+  // compress file or not
+  compress: false,
+
+  // out log or not
+  verbose: true
+};
+
+/**
+ * record last run time
+ * @type {WeakMap}
+ */
+const lastRunRecords = new WeakMap();
+
+/**
+ * return image optimize task
+ * @param {DEFAULT_OPTIONS} options option
+ * @return {Function}
+ * @example
+ * import {task} from 'gulp';
+ * import optimizeImage from '@hidoo/gulp-task-optimize-image';
+ *
+ * task('image', optimizeImage({
+ *   src: '/path/to/images/*.{jpg,jpeg,gif,png,svg,ico}',
+ *   dest: '/path/to/dest',
+ * }));
+ */
+export default function optimizeImage(options = {}) {
+  const opts = {...DEFAULT_OPTIONS, ...options},
+        isImage = filter('**/*.{jpg,jpeg,gif,png}', {restore: true}),
+        isSvg = filter('**/*.svg', {restore: true}),
+        isntIco = filter(['**', '!**/*.ico'], {restore: true});
+
+  // define task
+  // + record last run time to lastRunRecords that use for incremental build.
+  const task = () => {
+    const {evenize, evenizeOptions, compress, verbose} = opts,
+          {imageMagick} = evenizeOptions;
+
+    return src(opts.src, {since: lastRunRecords.get(task)})
+      .on('end', () => lastRunRecords.set(task, new Date()))
+      .pipe(plumber({errorHandler}))
+      .pipe(isImage)
+      .pipe(cond(evenize, evenizer({imageMagick, verbose})))
+      .pipe(isImage.restore)
+      .pipe(isntIco)
+      .pipe(cond(compress, imagemin({verbose})))
+      .pipe(isntIco.restore)
+      .pipe(dest(opts.dest))
+      .pipe(isSvg)
+      .pipe(cond(compress, gzip({append: true})))
+      .pipe(cond(compress, dest(opts.dest)))
+      .pipe(isSvg.restore);
+  };
+
+  return task;
+}
---
+++ packages/gulp-task-optimize-image/test/index.test.js
@@ -0,0 +1,92 @@
+/* eslint max-len: 0, no-magic-numbers: 0 */
+
+import assert from 'assert';
+import fs from 'fs';
+import rimraf from 'rimraf';
+import sizeOf from 'image-size';
+import optimizeImage from '../src';
+
+describe('gulp-task-optimize-image', () => {
+  const path = {
+    src: `${__dirname}/fixtures/src`,
+    dest: `${__dirname}/fixtures/dest`,
+    expected: `${__dirname}/fixtures/expected`
+  };
+
+  afterEach((done) =>
+    rimraf(`${path.dest}/*.{jpg,jpeg,png,gif,svg,svg.gz,ico}`, done)
+  );
+
+  it('should out to "options.dest" if argument "options.src" is set.', (done) => {
+    const task = optimizeImage({
+      src: `${path.src}/sample.{jpg,jpeg,png,gif,svg,ico}`,
+      dest: path.dest,
+      verbose: false
+    });
+
+    task().on('finish', () => {
+      ['jpg', 'png', 'gif', 'svg', 'ico'].forEach((ext) => {
+        const actual = fs.readFileSync(`${path.dest}/sample.${ext}`, {encode: null}),
+              expected = fs.readFileSync(`${path.expected}/sample.${ext}`, {encode: null});
+
+        assert(actual);
+        assert.deepEqual(actual, expected);
+      });
+      done();
+    });
+  });
+
+  it('should out evenized files to "options.dest" if argument "options.evenize" is true.', (done) => {
+    const cases = [
+            ['10x9.jpg', [10, 10]],
+            ['9x10.png', [10, 10]],
+            ['9x9.gif', [10, 10]]
+          ],
+          task = optimizeImage({
+            src: `${path.src}/{${cases.map(([file]) => file).join(',')}}`,
+            dest: path.dest,
+            evenize: true,
+            verbose: false
+          });
+
+    task().on('finish', () => {
+      cases.forEach(([file, [width, height]]) => {
+        const actual = sizeOf(`${path.dest}/${file}`);
+
+        assert(actual.width === width);
+        assert(actual.height === height);
+      });
+      done();
+    });
+  });
+
+  it('should out compressed files to "options.dest" if argument "options.compress" is true.', (done) => {
+    const task = optimizeImage({
+      src: `${path.src}/sample.{jpg,jpeg,png,gif,svg,ico}`,
+      dest: path.dest,
+      compress: true,
+      verbose: false
+    });
+
+    task().on('finish', () => {
+      ['jpg', 'png', 'gif', 'svg'].forEach((ext) => {
+        const actual = fs.readFileSync(`${path.dest}/sample.${ext}`, {encode: null});
+
+        if (ext === 'ico') {
+          const expected = fs.readFileSync(`${path.expected}/sample.${ext}`, {encode: null});
+
+          assert(actual);
+          assert.deepEqual(actual, expected);
+        }
+        else {
+          const expected = fs.readFileSync(`${path.expected}/sample.compressed.${ext}`, {encode: null});
+
+          assert(actual);
+          assert.deepEqual(actual, expected);
+        }
+      });
+      done();
+    });
+  });
+
+});

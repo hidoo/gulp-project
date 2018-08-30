22b0fbc7bef555d375421067de04fc6979f29a0a
marunagahideo <hidoooo@gmail.com>
2018-08-30 16:06:44 +0900

    feat(packages): add svg sprite sheet build task sample to gulp-project-sample

--- packages/gulp-project-sample/tasks/sprite.js
+++ packages/gulp-project-sample/tasks/sprite.js
@@ -4,6 +4,7 @@
 import {relative} from 'path';
 import gulp from 'gulp';
 import buildImageSprite from '@hidoo/gulp-task-build-sprite-image';
+import buildSvgSprite from '@hidoo/gulp-task-build-sprite-svg';
 
 /**
  * import modules - local
@@ -26,7 +27,7 @@ const pathToSpriteFromCss = relative(path.destCss, path.destSprite);
 /**
  * define tasks
  */
-export const main = buildImageSprite({
+export const image = buildImageSprite({
   src: `${path.srcSprite}/**/sample-*.png`,
   destImg: `${path.destSprite}`,
   destCss: `${path.srcCss}`,
@@ -36,6 +37,18 @@ export const main = buildImageSprite({
   evenize: false,
   compress
 });
+export const svg = buildSvgSprite({
+  src: `${path.srcSprite}/**/sample-*.svg`,
+  destImg: `${path.destSprite}`,
+  destCss: `${path.srcCss}`,
+  imgName: 'sample.svg',
+  cssName: '_sprite_svg_sample.styl',
+  imgPath: `${pathToSpriteFromCss}/sample.svg${cacheParameter}`,
+  evenize: false,
+  compress
+});
+export const main = gulp.parallel(image, svg);
 export const watch = () => {
-  gulp.watch(`${path.srcSprite}/**/sample-*.png`, main);
+  gulp.watch(`${path.srcSprite}/**/sample-*.png`, image);
+  gulp.watch(`${path.srcSprite}/**/sample-*.png`, svg);
 };

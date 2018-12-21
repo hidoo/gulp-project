640c211a39e7624e579d950990c6d3137a052583
marunagahideo <hidoooo@gmail.com>
2018-12-21 15:07:37 +0900

    chore(package): update @babel dependencies

--- packages/gulp-project-generator/src/generatePackageJson.js
+++ packages/gulp-project-generator/src/generatePackageJson.js
@@ -22,7 +22,7 @@ export default async function generatePackageJson(name = '', dest = '', options 
 
   const devDependencies = [
           {name: '@babel/core', version: '^7.2.2'},
-          {name: '@babel/preset-env', version: '^7.2.0'},
+          {name: '@babel/preset-env', version: '^7.2.3'},
           {name: '@babel/register', version: '^7.0.0'},
           {name: '@hidoo/eslint-config', version: '^0.1.3'},
           {name: '@hidoo/util-fancy-print', version: gulpProjectVersion},
@@ -75,7 +75,7 @@ export default async function generatePackageJson(name = '', dest = '', options 
   }
   if (options.js) {
     devDependencies.push(
-      {name: '@babel/polyfill', version: '^7.0.0'},
+      {name: '@babel/polyfill', version: '^7.2.3'},
       {name: 'babel-preset-power-assert', version: '^3.0.0'},
       {name: 'jsdom', version: '^12.2.0'},
       {name: 'jsdom-global', version: '^3.0.2'},

d4815e661508c3cd79310614a09a4f4ed1ea9fbe
marunagahideo <hidoooo@gmail.com>
2019-03-04 18:13:18 +0900

    chore(gulp-project-generator): update lint-staged to version 8.1.5

--- packages/gulp-project-generator/src/generatePackageJson.js
+++ packages/gulp-project-generator/src/generatePackageJson.js
@@ -32,7 +32,7 @@ export default async function generatePackageJson(name = '', dest = '', options 
           {name: 'eslint', version: '^5.15.0'},
           {name: 'gulp', version: '^4.0.0'},
           {name: 'husky', version: '^1.3.1'},
-          {name: 'lint-staged', version: '^8.1.0'},
+          {name: 'lint-staged', version: '^8.1.5'},
           {name: 'npm-run-all', version: '^4.1.5'},
           {name: 'rimraf', version: '^2.6.3'}
         ],

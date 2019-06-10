253d85fc1f6ce6c5a1bc2590c49df8017b13dce5
hidoo <hidoooo@gmail.com>
2019-06-10 18:50:40 +0900

    chore(gulp-project-generator): update eslint to version 5.16.0

--- packages/gulp-project-generator/src/generatePackageJson.js
+++ packages/gulp-project-generator/src/generatePackageJson.js
@@ -29,7 +29,7 @@ export default async function generatePackageJson(name = '', dest = '', options 
           {name: 'babel-eslint', version: '^10.0.1'},
           {name: 'commander', version: '^2.19.0'},
           {name: 'cross-env', version: '^5.2.0'},
-          {name: 'eslint', version: '^5.15.3'},
+          {name: 'eslint', version: '^5.16.0'},
           {name: 'gulp', version: '^4.0.0'},
           {name: 'husky', version: '^2.4.0'},
           {name: 'lint-staged', version: '^8.2.0'},

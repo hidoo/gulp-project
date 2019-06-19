25aa465420c96fd5e9a39fe6445c0d0eebf5e5f9
hidoo <hidoooo@gmail.com>
2019-06-19 12:12:38 +0900

    chore(gulp-project-generator): update husky to version 2.4.1

--- packages/gulp-project-generator/src/generatePackageJson.js
+++ packages/gulp-project-generator/src/generatePackageJson.js
@@ -31,7 +31,7 @@ export default async function generatePackageJson(name = '', dest = '', options 
           {name: 'cross-env', version: '^5.2.0'},
           {name: 'eslint', version: '^5.16.0'},
           {name: 'gulp', version: '^4.0.2'},
-          {name: 'husky', version: '^2.4.0'},
+          {name: 'husky', version: '^2.4.1'},
           {name: 'lint-staged', version: '^8.2.1'},
           {name: 'npm-run-all', version: '^4.1.5'},
           {name: 'rimraf', version: '^2.6.3'}

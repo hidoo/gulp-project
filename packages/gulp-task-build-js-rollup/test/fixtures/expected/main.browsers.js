92a1871bdb1eebf2e5ddf62e594e1a209abf96c2
hidoo <hidoooo@gmail.com>
2019-06-24 15:53:08 +0900

    test(build-js-rollup): fix test

--- packages/gulp-task-build-js-rollup/test/fixtures/expected/main.browsers.js
+++ packages/gulp-task-build-js-rollup/test/fixtures/expected/main.browsers.js
@@ -96,31 +96,9 @@
     return _assertThisInitialized(self);
   }
 
-  var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
-
-  function commonjsRequire () {
-  	throw new Error('Dynamic requires are not currently supported by rollup-plugin-commonjs');
-  }
-
-  function unwrapExports (x) {
-  	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
-  }
-
   function createCommonjsModule(fn, module) {
   	return module = { exports: {} }, fn(module, module.exports), module.exports;
   }
-
-  function getCjsExportFromNamespace (n) {
-  	return n && n['default'] || n;
-  }
-
-  var commonjsHelpers = /*#__PURE__*/Object.freeze({
-    commonjsGlobal: commonjsGlobal,
-    commonjsRequire: commonjsRequire,
-    unwrapExports: unwrapExports,
-    createCommonjsModule: createCommonjsModule,
-    getCjsExportFromNamespace: getCjsExportFromNamespace
-  });
 
   var _global = createCommonjsModule(function (module) {
     // https://github.com/zloirock/core-js/issues/86#issuecomment-115759028
@@ -211,7 +189,7 @@
       return store[key] || (store[key] = value !== undefined ? value : {});
     })('versions', []).push({
       version: _core.version,
-      mode: 'global',
+      mode:  'global',
       copyright: '© 2018 Denis Pushkarev (zloirock.ru)'
     });
   });

fed7c31a298c0a9f72af82629ecb71d71acbda40
hidoo <hidoooo@gmail.com>
2020-04-02 18:25:21 +0900

    chore(generator): update babel dependencies

--- packages/gulp-project-generator/src/generatePackageJson.js
+++ packages/gulp-project-generator/src/generatePackageJson.js
@@ -22,9 +22,9 @@ export default async function generatePackageJson(name = '', dest = '', options 
   const gulpProjectVersion = `^${pkg.version}`;
 
   const devDependencies = [
-          {name: '@babel/core', version: '^7.8.6'},
-          {name: '@babel/preset-env', version: '^7.8.6'},
-          {name: '@babel/register', version: '^7.8.6'},
+          {name: '@babel/core', version: '^7.9.0'},
+          {name: '@babel/preset-env', version: '^7.9.0'},
+          {name: '@babel/register', version: '^7.9.0'},
           {name: '@hidoo/eslint-config', version: '^0.4.0'},
           {name: '@hidoo/util-fancy-print', version: gulpProjectVersion},
           {name: 'commander', version: '^4.1.1'},

53382e06fd1de17b8cdc4992e0a29cf698e62ac0
hidoo <hidoooo@gmail.com>
2021-06-09 14:36:56 +0900

    chore(generator): update packages and examples

--- packages/gulp-project-generator/src/generatePackageJson.js
+++ packages/gulp-project-generator/src/generatePackageJson.js
@@ -119,7 +119,7 @@ export default async function generatePackageJson(name = '', dest = '', options 
   }
   if (options.server) {
     devDependencies.push(
-      {name: '@hidoo/express-engine-handlebars', version: '^0.8.1'},
+      {name: '@hidoo/express-engine-handlebars', version: '^0.8.2'},
       {name: '@hidoo/util-local-ip', version: gulpProjectVersion},
       {name: 'browser-sync', version: '^2.26.7'},
       {name: 'express', version: '^4.17.1'}
